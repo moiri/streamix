1abf4d9e86066610297e60407cb0934033716a4b
Simon Maurer <simon.maurer@humdek.unibe.ch>
2019-05-23 10:33:36 +0200

    adapt stream example to work with new rts

--- examples/stream/boximpl.c
+++ examples/stream/boximpl.c
@@ -5,16 +5,27 @@
 #include <zlog.h>
 #include <unistd.h>
 
-int a( void* handler )
+void* a_init( void* handler )
+{
+    (void)(handler);
+    printf( "enter any list of characters, followed by return\n" );
+    printf( "ESC, followed by return will terminate the application\n" );
+    return NULL;
+}
+
+void a_cleanup( void* state ) { (void)(state); }
+
+int a( void* handler, void* state )
 {
     char ch;
     char* data;
     smx_msg_t* msg;
+    (void)(state);
     if( read(STDIN_FILENO, &ch, 1) > 0 )
     {
         data = malloc( sizeof( char ) );
         *data = ch;
-        msg = smx_msg_create( data, sizeof( char ), NULL, NULL );
+        msg = SMX_MSG_CREATE( data, sizeof( char ), NULL, NULL, NULL );
         SMX_CHANNEL_WRITE( handler, a, x, msg );
         if( ch == ( char )27 )
             return SMX_NET_END;
@@ -22,12 +33,21 @@ int a( void* handler )
     return SMX_NET_CONTINUE;
 }
 
-int b( void* handler )
+void* b_init( void* handler )
+{
+    (void)(handler);
+    return NULL;
+}
+
+void b_cleanup( void* state ) { (void)(state); }
+
+int b( void* handler, void* state )
 {
     smx_msg_t* msg;
+    (void)(state);
     msg = SMX_CHANNEL_READ( handler, b, x );
     if( msg != NULL ) {
-        dzlog_info( "received data_x: %c", *( char* )msg->data );
+        printf( "received data: %c\n", *( char* )msg->data );
         SMX_MSG_DESTROY( msg );
         sleep(1);
     }
--- examples/stream/boximpl.h
+++ examples/stream/boximpl.h
@@ -1,7 +1,12 @@
 #ifndef BOXIMPL_H
 #define BOXIMPL_H
 
-int a( void* );
-int b( void* );
+void* a_init( void* );
+void a_cleanup( void* );
+int a( void*, void* );
+
+void* b_init( void* );
+void b_cleanup( void* );
+int b( void*, void* );
 
 #endif /* ifndef BOXIMPL_H */
